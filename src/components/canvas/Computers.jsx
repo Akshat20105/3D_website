91e6f0405458ae72f94b7e5984466ea3eaf7e66d
Akshat <akshatraj20105@gmail.com>
2024-07-06 11:33:46 +0530

    website prototype

---
+++ src/components/canvas/Computers.jsx
@@ -0,0 +1,53 @@
+import {Suspense,useEffect,useState} from 'react';
+import { Canvas } from '@react-three/fiber';
+import CanvasLoader from '../Loader';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+
+const Computers = ({isMobile}) => {
+  const computer= useGLTF('./desktop_pc/scene.gltf')
+  return (
+    <mesh>
+      <hemisphereLight intensity={2.5} groundColor="black"/>
+      <pointLight intensity={1.5}/>
+      <spotLight
+      position={[0, 10, 10]}
+      angle={0.8}
+      penumbra={2}
+      intensity={5}
+      castShadow
+      shadow-mapSize={1024}
+      />
+      <primitive object={computer.scene}
+      scale={isMobile ? 0.5 : 1 }
+      position={isMobile?[0,-1,2]:[0,-2.5,-2]}
+      rotation={[-0.01,-0.2,-0.1]}/>
+    </mesh>
+  )
+}
+const ComputersCanvas =() =>{
+  const [isMobile, setIsMobile]= useState(false);
+  useEffect(()=>{const mediaQuery= window.matchMedia('(max-width : 500px)');
+    setIsMobile(mediaQuery.matches);
+    const handleMediaQueryChange = (event)=>{setIsMobile(event.matches)}
+    mediaQuery.addEventListener('change',handleMediaQueryChange);
+    return ()=>{mediaQuery.removeEventListener('change',handleMediaQueryChange)}
+  },[])
+
+  return <Canvas frameloop='demand'
+  shadows 
+  camera={{position:[20,3,5],fov:25}}
+  gl={{preserveDrawingBuffer:true}}
+  >
+    <Suspense fallback={<CanvasLoader/>}>
+    <OrbitControls 
+    autoRotate autoRotateSpeed={2.5}
+    enableZoom={false} 
+    maxPolarAngle={Math.PI/2}
+    minPolarAngle={Math.PI/2}/>
+    <Computers/>
+    </Suspense>
+    <Preload all/>
+  </Canvas>
+}
+
+export default ComputersCanvas;
---
+++ src/constants/index.js
@@ -0,0 +1,265 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React Native Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Content Creator",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "Unity Game Developer",
+    company_name: "Game Development Club, IIT Kanpur",
+    icon: starbucks,
+    iconBg: "#383E56",
+    date: "June 2023 - March 2024",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "3D Artist",
+    company_name: "Design and Animation club, IIT Kanpur",
+    icon: tesla,
+    iconBg: "#E6DEDD",
+    date: "June 2023 - March 2024",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  // {
+  //   title: "Web Developer",
+  //   company_name: "Shopify",
+  //   icon: shopify,
+  //   iconBg: "#383E56",
+  //   date: "Jan 2022 - Jan 2023",
+  //   points: [
+  //     "Developing and maintaining web applications using React.js and other related technologies.",
+  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+  //     "Implementing responsive design and ensuring cross-browser compatibility.",
+  //     "Participating in code reviews and providing constructive feedback to other developers.",
+  //   ],
+  // },
+  // {
+  //   title: "Full stack Developer",
+  //   company_name: "Meta",
+  //   icon: meta,
+  //   iconBg: "#E6DEDD",
+  //   date: "Jan 2023 - Present",
+  //   points: [
+  //     "Developing and maintaining web applications using React.js and other related technologies.",
+  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+  //     "Implementing responsive design and ensuring cross-browser compatibility.",
+  //     "Participating in code reviews and providing constructive feedback to other developers.",
+  //   ],
+  // },
+];
+
+const testimonials = [
+  // {
+  //   testimonial:
+  //     "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+  //   name: "Sara Lee",
+  //   designation: "CFO",
+  //   company: "Acme Co",
+  //   image: "https://randomuser.me/api/portraits/women/4.jpg",
+  // },
+  // {
+  //   testimonial:
+  //     "I've never met a web developer who truly cares about their clients' success like Rick does.",
+  //   name: "Chris Brown",
+  //   designation: "COO",
+  //   company: "DEF Corp",
+  //   image: "https://randomuser.me/api/portraits/men/5.jpg",
+  // },
+  // {
+  //   testimonial:
+  //     "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+  //   name: "Lisa Wang",
+  //   designation: "CTO",
+  //   company: "456 Enterprises",
+  //   image: "https://randomuser.me/api/portraits/women/6.jpg",
+  // },
+];
+
+const projects = [
+  {
+    name: "Ignition Evade",
+    description:
+      "2D platformer Game, developed for Inter-IIT Tech meet 12.0 .",
+    tags: [
+      {
+        name: "Unity",
+        color: "blue-text-gradient",
+      },
+      {
+        name: " C#",
+        color: "green-text-gradient",
+      },
+      {
+        name: "FL Studio",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Chess Bot",
+    description:
+      "Chess game which allows P v/s P, P v/s Bot and Bot v/s Bot game modes, Bot evaluates the chessboard and plays the most optimized move, allows player to export the game to lichess.com.",
+    tags: [
+      {
+        name: "Unity",
+        color: "blue-text-gradient",
+      },
+      {
+        name: " C#",
+        color: "green-text-gradient",
+      },
+      // {
+      //   name: " ",
+      //   color: "pink-text-gradient",
+      // },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Temperature zones",
+    description:
+      "It's an educational game for middle to high school students, teaching them about Earth's temperature zones through interactive exploration.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
